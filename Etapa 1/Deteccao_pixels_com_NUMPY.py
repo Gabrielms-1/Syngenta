37b2fa07bf3811ed42f7f4f6d446f1f27e227bec
Gabrielms-1 <gabriel.sousa@syngenta.com>
2021-12-12 03:52:00 -0300

    Update Deteccao_pixels_com_NUMPY.py

--- Etapa 1/Deteccao_pixels_com_NUMPY.py
+++ Etapa 1/Deteccao_pixels_com_NUMPY.py
@@ -9,11 +9,11 @@ imageArray = np.array(im)
 #print(imageArray)
 
 # Organiza todos os pixels em matrizes [ix4] (x linhas e 3 colunas) e remove os valores duplicados
-colours, counts = np.unique(imageArray.reshape(-1,3), axis=0, return_counts=1)
+colors, counts = np.unique(imageArray.reshape(-1,3), axis=0, return_counts=1)
 
-print("Preto  = ", colours[0])
-print("Verde  = ", colours[1])
-print("Branco = ", colours[2])
+print("Preto  = ", colors[0])
+print("Verde  = ", colors[1])
+print("Branco = ", colors[2])
 
 print("Quantidade de pixels pretos:  " , counts[0])
 print("Quantidade de pixels verdes:  " , counts[1])
